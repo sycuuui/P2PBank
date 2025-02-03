b41ad23c3c6c7f8b9e9275fb9f82cf054759f10a
sycuuui <102959791+sycuuui@users.noreply.github.com>
2025-02-03 18:54:58 +0900

    #1 chore:프로젝트 생성

---
+++ src/test/java/bank/p2pbank/P2PBankApplicationTests.java
@@ -0,0 +1,13 @@
+package bank.p2pbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P2PBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
