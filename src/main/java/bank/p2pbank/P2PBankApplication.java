3274d9505d550dfef7145af9e619f7b390bd3a9a
sycuuui <102959791+sycuuui@users.noreply.github.com>
2025-02-04 22:39:08 +0900

    #2 feat: BaseTimeEntity 구현

--- src/main/java/bank/p2pbank/P2PBankApplication.java
+++ src/main/java/bank/p2pbank/P2PBankApplication.java
@@ -2,7 +2,9 @@ package bank.p2pbank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing //JPA Auditing 기능 활성화를 위해
 @SpringBootApplication
 public class P2PBankApplication {
 
