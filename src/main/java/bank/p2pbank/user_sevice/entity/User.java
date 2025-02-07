7963d7957cef9853dafb123c967476a0e39b576b
sycuuui <102959791+sycuuui@users.noreply.github.com>
2025-02-07 16:32:55 +0900

    #2 feat: User entity 구현 추가

--- src/main/java/bank/p2pbank/user_sevice/entity/User.java
+++ src/main/java/bank/p2pbank/user_sevice/entity/User.java
@@ -1,8 +1,10 @@
 package bank.p2pbank.user_sevice.entity;
 
 import bank.p2pbank.common.BaseTimeEntity;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.NoArgsConstructor;
 
 @Entity
@@ -14,12 +16,20 @@ public class User extends BaseTimeEntity {
     @Column(name = "id", nullable = false, columnDefinition = "bigint")
     private Long id;
 
-    @Column(name = "email", nullable = false, columnDefinition = "varchar(320)")
+    @Column(name = "email", nullable = false, columnDefinition = "varchar(320)", unique = true) //unique- 중복불가
     private String email;
 
-    @Column(name = "password", columnDefinition = "varchar(255)")
+    @JsonIgnore //JSON 직렬화에서 제외한다
+    @Column(name = "password",nullable = false, columnDefinition = "varchar(255)")
     private String password;
 
     @Column(name = "name", nullable = false, columnDefinition = "varchar(50)")
     private String name;
+
+    @Builder
+    public User(String email, String password, String name) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+    }
 }
