2618196d7c5595ec2c98916746900ec07f935c65
sycuuui <102959791+sycuuui@users.noreply.github.com>
2025-03-02 15:21:57 +0900

    #6 feat: 비밀번호 최소 길이 검증 어노테이션 추가

--- src/main/java/bank/p2pbank/domain/user/dto/request/RegisterRequest.java
+++ src/main/java/bank/p2pbank/domain/user/dto/request/RegisterRequest.java
@@ -3,11 +3,12 @@ package bank.p2pbank.domain.user.dto.request;
 import bank.p2pbank.domain.user.enumerate.Role;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public record RegisterRequest(
         @NotNull @Email
         String email,
-        @NotNull
+        @NotNull @Size(min = 8, message = "비밀번호는 최소 8자 이상이어야 합니다.")
         String password,
         @NotNull
         String name,
