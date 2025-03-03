7d63999e395d8bed2a7b2d012ffdf5fd597aa5ac
sycuuui <102959791+sycuuui@users.noreply.github.com>
2025-03-03 12:39:49 +0900

    #13 docs: 어노테이션 주석 추가

--- src/test/java/bank/p2pbank/global/config/TestContainerConfig.java
+++ src/test/java/bank/p2pbank/global/config/TestContainerConfig.java
@@ -8,13 +8,13 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Testcontainers
 public abstract class TestContainerConfig {
-    @Container
+    @Container //mysql 컨테이너 정의
     static MySQLContainer<?> mysqlContainer = new MySQLContainer<>("mysql:8.0.30")
             .withDatabaseName("testdb")
             .withUsername("testuser")
             .withPassword("testpass");
 
-    @DynamicPropertySource
+    @DynamicPropertySource //컨테이너 가동시, 자동으로 springboot에 자동으로 주입
     static void setDatasourceProperties(DynamicPropertyRegistry registry) {
         registry.add("spring.datasource.url", mysqlContainer::getJdbcUrl);
         registry.add("spring.datasource.username", mysqlContainer::getUsername);
