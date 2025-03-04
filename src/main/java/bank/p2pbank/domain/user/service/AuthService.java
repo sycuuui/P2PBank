12aa3defdbf1e112d6a045967456b13387a93a2f
sycuuui <102959791+sycuuui@users.noreply.github.com>
2025-03-04 14:10:24 +0900

    #6 feat: 비밀번호 오류 시 log 코드 추가

--- src/main/java/bank/p2pbank/domain/user/service/AuthService.java
+++ src/main/java/bank/p2pbank/domain/user/service/AuthService.java
@@ -60,7 +60,7 @@ public class AuthService {
         }
 
         if (!passwordEncoder.matches(loginRequest.password(), user.get().getPassword())) {
-            log.warn("올바르지 않은 비밀번호. email={}", loginRequest.password());
+            log.warn("올바르지 않은 비밀번호. password={}", loginRequest.password());
             throw new ApplicationException(ErrorCode.WRONG_PASSWORD_EXCEPTION);
         }
 
